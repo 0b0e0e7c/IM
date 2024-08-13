7ad03b7a8a24b88c4bad9a75c4f4c46ff818f4d6
Jackey <jackey@hknova.com>
2024-08-13 16:45:22 +0800

    chore: better tasks

--- friend-service/cmd/rpc/friend.go
+++ friend-service/cmd/rpc/friend.go
@@ -16,7 +16,7 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-var configFile = flag.String("f", "etc/friend.yaml", "the config file")
+var configFile = flag.String("f", "friend-service/cmd/rpc/etc/friend.yaml", "the config file")
 
 func main() {
 	flag.Parse()
